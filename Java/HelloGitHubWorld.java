6a5c24c2046b152436b5251f1083a223a3e642b9
Rachel Barker <rachelbarker4x4@gmail.com>
2018-10-15 12:20:23 -0700

    Add HelloGitHubWorld.java
    Another Hello World file in Java.

---
+++ Java/HelloGitHubWorld.java
@@ -0,0 +1,8 @@
+public class HelloGitHubWorld {
+
+    public static void main(String[] args) {
+        // Prints greeting to the java terminal window.
+        System.out.println("Hello, World");
+		System.out.println("Happy Hacktoberfest from Rachel B in California, USA!");
+
+}
